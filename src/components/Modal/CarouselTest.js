627ff7563a0a34a4d30ce2bc108b03b68442bf5c
Federico_531 <federicomarchetta@yahoo.com.ar>
2020-08-13 15:14:46 -0300

    investigar como implementar gatsby

--- src/components/Modal/CarouselTest.js
+++ src/components/Modal/CarouselTest.js
@@ -1,57 +1,9 @@
-import React from 'react';
-import {Carousel} from 'reactstrap'
-export default class Carousel extends React.Component {
+//CONVERTIR REACT PROJECT TO GATSBY
+//https://www.youtube.com/watch?v=2E6AiY5chgM
 
+//Instalar GATSBY 
+//https://www.youtube.com/watch?v=b2H7fWhQcdE
 
-    render() {
-        function ControlledCarousel() {
-            const [index, setIndex] = useState(0);
-          
-            const handleSelect = (selectedIndex, e) => {
-              setIndex(selectedIndex);
-            };
-          
-        return (
-            <>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=First slide&bg=373940"
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Second slide&bg=282c34"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Third slide&bg=20232a"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </>
-        )
-    }
-}
+//HACER CAROUSEL con bootstrap y gatsby
+// https://www.youtube.com/watch?v=M8ZDDbEGrBA
 
