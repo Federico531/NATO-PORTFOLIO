6d0f0bd13925d70e6e2da6a115790b3e4cba37d6
unknown <federicomarchetta@yahoo.com.ar>
2020-08-18 20:43:35 -0300

    no puedo hacer modal mas largo

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,12 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
 import React, { useState } from 'react';
+import PropTypes from 'prop-types'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const ModalExample = (props) => {
+
+
   const {
     buttonLabel,
     className
@@ -31,21 +34,16 @@ const ModalExample = (props) => {
 
       <img id={props.id} className='image' onClick={toggle} src={props.url} alt="whatever" />
       <div className="middle">
-        <div className="text">hola
+        <div className="text">hola  {/*EL CHILDREN PUEDE SER UNA PROPIEDAD EN PORTFOLIO 1 Y GRID 1X3 */}
                         {/* {props.img_text} */}
         </div>
       </div>
 
-      <Modal isOpen={modal} toggle={toggle} className={className} backdrop={backdrop} keyboard={keyboard}>
-        <ModalHeader style={{ paddingLeft: '42.5%' }} toggle={toggle}>Galeria</ModalHeader>
-        <ModalBody>
+      <Modal  style={{marginTop:'5%', minWidth:'90%'}}isOpen={modal} toggle={toggle} className='modalito' backdrop={backdrop} keyboard={keyboard}>
+        <ModalBody style={{padding:'0'}}>
           {props.children}
         </ModalBody>
-        <ModalFooter>
-          <div style={{ marginRight: '44%' }}>
-            <Button color="danger" onClick={toggle} className='text-left'>Volver</Button>
-          </div>
-        </ModalFooter>
+
       </Modal>
     </div>
   );
